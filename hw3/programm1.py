9f862f31b633cf1f52a638e9e25e98265d3caa98
Василиса <vasilyoook@mail.ru>
2016-10-17 22:00:39 +0300

    python

---
+++ hw3/programm1.py
@@ -0,0 +1,14 @@
+print ('Дает ли а остаток с при делении на b')
+a = int(input ("a= "))
+b = int(input ("b= "))
+c = int(input ("c= "))
+if a % b == c:
+    print('Да')
+else:
+    print('Нет')
+
+print('Если а разделить на b получится ли с')
+if a / b == c:
+        print('Да')
+else:
+        print('Нет') 
